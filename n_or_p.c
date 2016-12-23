2e25eb725bffa5ca28dfb4e9b98ef7636401dc53
vigneshvikas <vikeyvikas97v@gmail.com>
2016-12-23 00:56:37 -0800

    Add files via upload

---
+++ n_or_p.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+int a;
+printf("enter a number");
+scanf("%d",&a);
+if(a>0)
+printf("it is positive");
+else if(a==0)
+printf("it is zero ");
+else
+    printf("it is negative n.0");
+return 0;
+}
