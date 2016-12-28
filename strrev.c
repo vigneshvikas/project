9880126d299fef11c41eeba1c56e317d46343b67
vigneshvikas <vikeyvikas97v@gmail.com>
2016-12-28 02:11:36 -0800

    Add files via upload

---
+++ strrev.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main(){
+char a[10],b[10];
+int l,j,i,p=0;
+printf("enrter a string");
+scanf("%s",a);
+l=strlen(a);
+j=l-1;
+for(i=0;i<=l;i++)
+{
+b[j]=a[i];
+j--;
+}
+for(i=0;i<l;i++){
+printf("%c",b[i]);
+}
+}
+
